402db506b3c16afd260e8130f41823dd0349d9d3
priyankacute <bhavanimurugesh2000@gmail.com>
2023-10-21 06:53:02 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let a=6
+let b=prompt("enter the number")
+let sum=function(a,b){
+    
+    if(a==b){
+        document.write("good work")
+    }
+
+    else{
+        document.write("not matched")
+    }
+}
+sum(a,b)
